9834fdbc74505ba73f85bd08651d38822e48cb20
Cirulei <r223730@dac.unicamp.br>
2025-09-13 15:04:08 -0300

    [Fix] Fixed for loop to write data in sd card.

--- data_logger/data_logger.ino
+++ data_logger/data_logger.ino
@@ -6,13 +6,13 @@
 
 // MICRO SD
 File myFile;  // Define myfile variable
-String fname = "test10.txt";
+String fname = "testvai.txt";
 const int chipSelect = 10;
 
 // Timer
-const uint8_t int_period = 10; // Each 10ms
-uint8_t count = 0;
-bool av_read_acc = false;
+const uint8_t int_period = 5; // Each 10ms
+const uint8_t sample_time_ms = 100/int_period; // Define sample time
+volatile uint8_t count = 0;
 
 // ACC
 const int MPU = 0x68;
@@ -23,30 +23,19 @@ const uint8_t num_cols = 7;
 uint8_t last_idx = 0;
 String aux_ = "";
 
-const uint8_t acc_data_size = num_cols * 3;
+const uint8_t acc_data_size = num_cols * 4;
 float acc_data[acc_data_size] = {};  // To save 280ms of data
 
 const uint8_t max_j = num_cols - 1;
 const uint8_t max_i = acc_data_size - num_cols;
 
-ISR(TIMER0_COMPB_vect) {
-  OCR0B += 156;  // Advance The COMPA Register
+ISR(TIMER0_COMPA_vect) {
+  OCR0A += 156;  // Advance The COMPA Register
   count += 1;
-
-  if (count % 10 == 0) {  // Each 50ms
-    av_read_acc = true;
-  }
 }
 
 void setup() {
-  delay(3000);
-  // TIMER
-  TCCR0A = 0;           // Init Timer0A
-  TCCR0B = 0;           // Init Timer0B
-  TCCR0B |= B00000101;  // Prescaler = 1024
-  OCR0B = 156;          // Timer Compare0B Register
-  TIMSK0 |= B00000100;  // Enable Timer COMPB Interrupt
-
+  delay(5000);
 
   Serial.begin(9600);
   // MICRO SD
@@ -58,24 +47,26 @@ void setup() {
     return;
   }
   Serial.println("initialization done.");
+
+  // TIMER
+  TCCR0A = 0;           // Init Timer0A
+  TCCR0B = 0;           // Init Timer0B
+  TCCR0B |= B00000101;  // Prescaler = 1024
+  OCR0A = 156;        // Timer Compare0A Register
+  TIMSK0 |= B00000010;  // Enable Timer COMPA Interrupt
 }
 
 void loop() {
   // Read acc
-  if (av_read_acc) {
-    av_read_acc = false;
+  if (count >= sample_time_ms) {
+    //av_read_acc = false;
     read_acc();
-    last_idx += 7;
+    last_idx += num_cols;
   }
 
   if (last_idx == acc_data_size) {
-    //int count_init = count;
     write_sd();
     last_idx = 0;
-    //count_init = count - count_init;
-    //if (count_init >= 4) {
-    //  Serial.println("time: " + String(count_init));
-    //}
   }
 }
 
@@ -100,7 +91,7 @@ void write_sd() {
 
   // if the file opened okay, write to it:
   if (myFile) {
-    for (uint8_t i = 0; i < max_i; i += num_cols) {
+    for (uint8_t i = 0; i <= max_i; i += num_cols) {
       for (uint8_t j = 0; j < num_cols; j++) {
         aux_ += String(acc_data[i + j], 2) + ",";
       }
@@ -112,6 +103,6 @@ void write_sd() {
     myFile.close();
   } else {
     // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
+    Serial.println("error opening file");
   }
 }
